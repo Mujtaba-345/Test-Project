6981c8c5d28a276779099463da8248307d101f80
Mujtaba <mujtaba@tripledsystems.com>
2024-01-02 17:58:46 +0500

    update settings files

--- test_project/settings.py
+++ test_project/settings.py
@@ -65,22 +65,22 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Mujtaba405$Test_Proj',
-        'USER': 'Mujtaba405',
-        'PASSWORD': 'YES',
-        'HOST': 'Mujtaba405.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Mujtaba405$Test_Proj',
+#         'USER': 'Mujtaba405',
+#         'PASSWORD': 'YES',
+#         'HOST': 'Mujtaba405.mysql.pythonanywhere-services.com',
+#         'PORT': '3306',
+#     }
+# }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
