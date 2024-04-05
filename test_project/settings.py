3039df3f0ffda375aa147af114d61e1bd48886e5
Malik Mujtaba <132654462+Mujtaba-345@users.noreply.github.com>
2024-04-05 14:12:45 +0500

    Update settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -200,7 +200,7 @@ SWAGGER_SETTINGS = {
 if app_env=="prod":
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = "shehroz-bucket"
+    AWS_STORAGE_BUCKET_NAME =os.environ.get("AWS_STORAGE_BUCKET_NAME")
     AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
